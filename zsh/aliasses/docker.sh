2ffd088235a25061627622ec9f641e76fa3c9625
Siebren Zwerver <siebren@siebjee.nl>
2021-09-22 14:09:53 +0200

    add docker aliasses

---
+++ zsh/aliasses/docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## Basic docker commands
+alias d="docker"
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+
+## Docker image commands
+alias dls="docker container ls"
+alias dlsa="docker container ls -a"
+
+## Docker image commands
+alias dils="docker image ls"
+
+## Docker volume commands
+alias dvls="docker volume ls"
+
+## Docker network commands
+alias dnls="docker network ls"
+
+## Docker build commands
+alias db="docker build"
+alias dbf="docker build --progress=plain"
