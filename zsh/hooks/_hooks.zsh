a6be39dd92cd02c288b19dc14c9fce2727ba4164
Siebren Zwerver <siebren@siebjee.nl>
2021-10-05 13:50:59 +0200

    remove env reload on dir change

--- zsh/hooks/_hooks.zsh
+++ zsh/hooks/_hooks.zsh
@@ -1,5 +1,4 @@
 chpwd()
-  _env_reload
   case $PWD in
   ( "${HOME}/projects/personal/configuration"  ||  "${HOME}/projects/personal/configuration/"* )
     source ${TOOLING_ZSH_DIR}/hooks/demo.zsh
@@ -8,4 +7,3 @@ chpwd()
     source ${TOOLING_ZSH_DIR}/hooks/default.zsh
   ;;
   esac
-
