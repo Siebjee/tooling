eddf29b0ded75e788eb9a645d7593cebc0683b4a
Siebren Zwerver <siebren@siebjee.nl>
2022-08-02 15:35:04 +0200

    Fix tux

--- zsh/functions/tmux.sh
+++ zsh/functions/tmux.sh
@@ -1,17 +1,19 @@
 function tux {
     local session=$1
 
-    tmux attach-session -t ${session} -f ${TOOLING_BASE_DIR}/tmux/tmux.conf \
-    || tmux new-session -d -s ${session} -f ${TOOLING_BASE_DIR}/tmux/tmux.conf
-
-    if [ -f "${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux" ]; then
-        if [[ ! -x "${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux" ]]; then
-            chmod +x "${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux"
+    if [ $(tmux list-sessions | grep ${session} | wc -l) = 1 ]; then
+        tmux attach-session -t ${session} -f ${TOOLING_BASE_DIR}/tmux/tmux.conf
+    else
+        tmux new-session -d -s ${session} -f ${TOOLING_BASE_DIR}/tmux/tmux.conf
+        if [ -f "${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux" ]; then
+            if [[ ! -x "${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux" ]]; then
+                chmod +x "${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux"
+            fi
+            session=${session} . ${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux
         fi
-        session=${session} . ${TOOLING_BASE_DIR}/tmux/myconfig/${session}.tmux
-    fi
 
-    tmux attach-session -t ${session}:0
+        tmux attach-session -t ${session}:0
+    fi
 }
 
 function _tux_completions {
