7848d4b48b244483f900a84d1642ec55a644db33
Siebren Zwerver <siebren@siebjee.nl>
2023-06-08 11:44:48 +0200

    Add less pager for AWS

--- zsh/hooks/default.zsh
+++ zsh/hooks/default.zsh
@@ -1,2 +1,3 @@
 unset DEMO_VAR
 export BUILDKIT_STEP_LOG_MAX_SIZE=10000000
+export AWS_PAGER='less --LONG-PROMPT --tabs=3 --quit-at-eof --quit-if-one-screen --tilde --jump-target=3 --ignore-case --status-column'
